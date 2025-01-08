8a38c21ac414564696e74edf8ecbc0ab16bf77a0
akktii <aktan.m@reviro.io>
2025-01-09 00:08:34 +0600

    create express app & apply nodemon

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let a = 5
-let b = 10
+import express from 'express'
 
-console.log(a + b, 'hello');
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('11 hello world')
+})
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);  
+    } 
+
+    console.log('server is ok');
+})
