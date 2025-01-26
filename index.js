bddbce964d8b9612571fb6fb40d4a6c89c59bcc9
akktii <aktan.m@reviro.io>
2025-01-26 22:53:55 +0600

    connect db & ability to reg and auth the user

--- index.js
+++ index.js
@@ -1,15 +1,110 @@
-import express from 'express'
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
 
-const app = express()
+import { registerValidation } from "./validations/auth.js";
 
-app.get('/', (req, res) => {
-    res.send('11 hello world')
-})
+import UserModel from "./models/User.js";
 
-app.listen(4444, (err) => {
-    if (err) {
-        return console.log(err);  
-    } 
+mongoose
+  .connect(
+    "mongodb+srv://akktii:aktan010104@cluster0.julvv.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("DB is ok");
+  })
+  .catch((err) => console.log("DB error", err));
 
-    console.log('server is ok');
-})
+const app = express();
+
+app.use(express.json());
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Не удалось зарегистрироваться" });
+  }
+});
+
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Неверный логин или пароль",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Не удалось авторизоваться" });
+  }
+});
+
+app.listen(4444, (error) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log("server is ok");
+});
